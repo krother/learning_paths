d0d2482dbd06c1c6ceef0393748dde906362a9c6
Kristian Rother <kristian@spiced-academy.com>
2020-07-15 21:22:07 +0200

    add build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+mkdir -p pdfs
+
+pandoc command_line.md -o pdfs/command_line.pdf
+pandoc machine_learning.md -o pdfs/machine_learning.pdf
+pandoc python.md -o pdfs/python.pdf
+pandoc sql.md -o pdfs/sql.pdf
+pandoc git.md -o pdfs/git.pdf
+pandoc pandas.md -o pdfs/pandas.pdf
